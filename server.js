1e3eff1fb3fab8ab3e5b789d4bc9b6b071e2082f
Iwan <iwan.cole.2015@bristol.ac.uk>
2017-09-07 22:37:15 +0100

    Added server-side validation

--- server.js
+++ server.js
@@ -50,20 +50,15 @@ var write_log = function(text) {
 };
 
 
-// Basic python spawner, needs LOTS of work
+// Python cild process spawner
 var python_intent = function(userReq, type) {
     var pyHandler = spawn("python", ["pixel_handler.py"]);
     var pyOutput = "";
     var pixelCode = lookup_code(userReq);
     var newIntent = intents[intents.length - 1] + 1;
     intents.push(newIntent);
-
-    if (type == 2) {
-        globalBrightness = pixelCode.replace("b","");
-    }
-
+    if (type == 2) { globalBrightness = pixelCode.replace("b","") }
     var packet = {"iid":newIntent, "type":type.toString(), "value":pixelCode}
-
     pyHandler.stdout.on("data", function(data) {
         pyOutput += data.toString();
     });
@@ -86,7 +81,7 @@ var make_hash = function(input) {
 
 // I know this isn't secure, but think about the project's context
 // Used to determine between clients that have both been Auth'd, but
-// one was Auth'd in a previous server session, so is an old client
+// the client was Auth'd in a previous server session, no longer valid
 var server_session = function() {
     var seed = Math.floor(Math.random() * 100) + 1;
     session_key = make_hash(seed);
@@ -124,105 +119,131 @@ var server_sweep = function() {
 };
 
 
+// Validate the data sent by the client
+var validate_request = function(body) {
+    keys = Object.keys(body);
+    if (keys.length < 1 || keys.length > 5) { return false }
+    var acceptedKeys = ['colour', 'effect_type', 'id', 'level', 'passcode', 'server_key', 'type'];
+    // At most O(7n) runtime as accepted keys is fixed
+    for (var i = 0; i < keys.length; i++) {
+        var userKey = keys[i];
+        var inList = false;
+        for (var j = 0; j < acceptedKeys.length; j++) {
+            inList = inList | (userKey == acceptedKeys[j]);
+        }
+        if (!inList) { return false }
+        if (userKey == "id" || userKey == "server_key" || userKey == "passcode") {
+            if (body[userKey] == undefined) { return false }
+        }
+        var acceptedKeyPairs = {'get_uid':1, 'get_brightness':1, 'intent_auth':1, 'intent_colour':1, 'intent_effect':1, 'intent_bright':1, 'intent-off':1};
+        if (opcodes[body[userKey]] == undefined && body[userKey] == undefined) { return false }
+    }
+    return true;
+};
+
+
 // POST method route
 app.post('/', function (req, res) {
-    var type = req.body.type;
+    if (validate_request(req.body) == true) {
+        var type = req.body.type;
 
-    // Client requests a UID
-    if (type == "get_uid") {
-        var newID = UIDs[UIDs.length - 1] + 1;
-        UIDs.push(newID);
-        responseUID = make_hash(newID);
+        // Client requests a UID
+        if (type == "get_uid") {
+            var newID = UIDs[UIDs.length - 1] + 1;
+            UIDs.push(newID);
+            responseUID = make_hash(newID);
 
-        var response = '{"type":"get","newID":"' + responseUID + '"}';
-        write_log("["+ responseUID.substr(0, 5) + "] GET_UID");
+            var response = '{"type":"get","newID":"' + responseUID + '"}';
+            write_log("["+ responseUID.substr(0, 5) + "] GET_UID");
+        }
+
+        if (type == "get_brightness") {
+            var valid = valid_intent(req.body.id, req.body.server_key);
+            if (valid == true) {
+                write_log("[" + req.body.id.substr(0,5) + "] GET_BRIGHT");
+                var response = '{"type":"get","success":1,"level":"' + globalBrightness + '"}';
+            }
+            else {
+                write_log("[" + req.body.id.substr(0,5) + "] GET_BRIGHT: BAD AUTH");
+                var response = '{"type":"get","success":"false"}';
+            }
+        }
+
+        // Client attempts authentication
+        else if (type == "intent_auth") {
+            var passHash = make_hash(req.body.passcode);
+            var success = valid_pass(passHash).toString();
+            var hashID = req.body.id;
+            write_log("[" + hashID.substr(0,5) + "] INTENT_AUTH: " + success);
+            if (success == "true") {
+                UIDs_Auth.push(hashID);
+                var response = '{"type":"intent","success":1,"session":"'+ session_key +'"}';
+            } else {
+                var response = '{"type":"intent","success":"false"}';
+            }
+        }
+
+        // Client selects a colour
+        else if (type == "intent_colour") {
+            var valid = valid_intent(req.body.id, req.body.server_key);
+            if (valid == true) {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_COLOUR: " + req.body.colour);
+                var colourSelected = req.body.colour;
+                var response = '{"type":"intent","success":1,"colour":"' + colourSelected + '"}';
+                python_intent(req.body.colour, 0);
+            }
+            else {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_COLOUR: BAD AUTH");
+                var response = '{"type":"intent","success":"false"}';
+            }
+        }
+
+        else if (type == "intent_effect") {
+            var valid = valid_intent(req.body.id, req.body.server_key);
+            if (valid == true) {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_EFFECT: " + req.body.effect_type);
+                var effectSelected = req.body.effect_type;
+                var response = '{"type":"intent","success":1,"effect":"' + effectSelected + '"}';
+                python_intent(effectSelected, 1);
+            }
+            else {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_EFFECT: BAD AUTH");
+                var response = '{"type":"intent","success":"false"}';
+            }
+        }
+
+        else if (type == "intent_bright") {
+            var valid = valid_intent(req.body.id, req.body.server_key);
+            if (valid == true) {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_BRIGHT: " + req.body.level);
+                var brightLevel = req.body.level;
+                // globalBrightness = brightLevel;
+                var response = '{"type":"intent","success":1,"level":"' + brightLevel + '"}';
+                python_intent(brightLevel, 2);
+            }
+            else {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_EFFECT: BAD AUTH");
+                var response = '{"type":"intent","success":"false"}';
+            }
+        }
+
+        // Client selects power off (for the LEDs)
+        else if (type == "intent_off") {
+            var valid = valid_intent(req.body.id, req.body.server_key);
+            if (valid == true) {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_OFF");
+                var response = '{"type":"intent","success":1}';
+                python_intent("off", 0);
+            }
+            else {
+                write_log("[" + req.body.id.substr(0,5) + "] INTENT_OFF: BAD AUTH");
+                var response = '{"type":"intent","success":"false"}';
+            }
+        }
     }
-
-    if (type == "get_brightness") {
-        var valid = valid_intent(req.body.id, req.body.server_key);
-        if (valid == true) {
-            write_log("[" + req.body.id.substr(0,5) + "] GET_BRIGHT");
-            var response = '{"type":"get","success":1,"level":"' + globalBrightness + '"}';
-        }
-        else {
-            write_log("[" + req.body.id.substr(0,5) + "] GET_BRIGHT: BAD AUTH");
-            var response = '{"type":"get","success":"false"}';
-        }
+    else {
+        var response = '{"type":"server","success":"false"}';
     }
-
-    // Client attempts authentication
-    else if (type == "intent_auth") {
-        var passHash = make_hash(req.body.passcode);
-        var success = valid_pass(passHash).toString();
-        var hashID = req.body.id;
-        write_log("[" + hashID.substr(0,5) + "] INTENT_AUTH: " + success);
-        if (success == "true") {
-            UIDs_Auth.push(hashID);
-            var response = '{"type":"intent","success":1,"session":"'+ session_key +'"}';
-        } else {
-            var response = '{"type":"intent","success":"false"}';
-        }
-    }
-
-    // Client selects a colour
-    else if (type == "intent_colour") {
-        var valid = valid_intent(req.body.id, req.body.server_key);
-        if (valid == true) {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_COLOUR: " + req.body.colour);
-            var colourSelected = req.body.colour;
-            var response = '{"type":"intent","success":1,"colour":"' + colourSelected + '"}';
-            python_intent(req.body.colour, 0);
-        }
-        else {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_COLOUR: BAD AUTH");
-            var response = '{"type":"intent","success":"false"}';
-        }
-    }
-
-    else if (type == "intent_effect") {
-        var valid = valid_intent(req.body.id, req.body.server_key);
-        if (valid == true) {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_EFFECT: " + req.body.effect_type);
-            var effectSelected = req.body.effect_type;
-            var response = '{"type":"intent","success":1,"effect":"' + effectSelected + '"}';
-            python_intent(effectSelected, 1);
-        }
-        else {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_EFFECT: BAD AUTH");
-            var response = '{"type":"intent","success":"false"}';
-        }
-    }
-
-    else if (type == "intent_bright") {
-        var valid = valid_intent(req.body.id, req.body.server_key);
-        if (valid == true) {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_BRIGHT: " + req.body.level);
-            var brightLevel = req.body.level;
-            // globalBrightness = brightLevel;
-            var response = '{"type":"intent","success":1,"level":"' + brightLevel + '"}';
-            python_intent(brightLevel, 2);
-        }
-        else {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_EFFECT: BAD AUTH");
-            var response = '{"type":"intent","success":"false"}';
-        }
-    }
-
-
-    // Client selects power off (for the LEDs)
-    else if (type == "intent_off") {
-        var valid = valid_intent(req.body.id, req.body.server_key);
-        if (valid == true) {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_OFF");
-            var response = '{"type":"intent","success":1}';
-            python_intent("off", 0);
-        }
-        else {
-            write_log("[" + req.body.id.substr(0,5) + "] INTENT_OFF: BAD AUTH");
-            var response = '{"type":"intent","success":"false"}';
-        }
-    }
-
     res.send(response);
 })
 
